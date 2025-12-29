99c5b08937f40ab50d22449e93a5c540fec2371a
maxswd-wincl <max@wincl.io>
2025-12-29 12:21:14 +0500

    Fix formatting of print statement in FormulaCreator

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -5,7 +5,7 @@ public class FormulaCreator {
         char a = 'a';
         char b = 'b';
 
-        System.out.println( (9 * (a*a) - 5 * b + 2 + a - 7) * ((a+b-4*a*b)/2)   )
+        System.out.println( (9 * (a*a) - 5 * b + 2 + a - 7) * ((a+b-4*a*b)/2));
         
     }
 }
