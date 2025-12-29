67733345bd03786b25fc7268743e37df30165b94
maxswd-wincl <max@wincl.io>
2025-12-29 12:15:35 +0500

    Update IntRepresentation.java

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 'c';
+        char h = 'h';
+        char a = 'a';
+        char r = 'r';
+
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(r);
     }
 }
